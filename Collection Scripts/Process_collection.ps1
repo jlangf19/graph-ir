26d810ff00664d84d0b058317ea51a83f6e3882c
jlangf19 <108038851+jlangf19@users.noreply.github.com>
2022-11-25 21:49:44 -0500

    Update Process_collection.ps1

--- Collection Scripts/Process_collection.ps1
+++ Collection Scripts/Process_collection.ps1
@@ -1,4 +1,11 @@
-﻿$procs = Get-CimInstance -Class Win32_Process | Select-Object -Property name, processid,parentprocessid,ExecutablePath, creationdate
+<#
+Author: Joshua Langford
+Description: This script collects metadata of Windows process objects and selects specific properties related to forensic analysis.
+Output:
+    Type: CSV
+    Attributes: Name, executable path, PID, parent PID, and creation time(all in Neo4j time formatting)
+#>
+$procs = Get-CimInstance -Class Win32_Process | Select-Object -Property name, processid,parentprocessid,ExecutablePath, creationdate
 foreach($x in $procs){
     $x | Add-Member (('{0:yyyyMMdd}' -f $x.creationdate).ToString() + "T" + ('{0:HH:mm}' -f $x.creationdate).ToString() + "Z") -Name neo4jcreationtime -MemberType NoteProperty
 }
