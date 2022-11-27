ce880fd2c42c7ba4a90f88789fea1fb48f055ead
jlangf19 <108038851+jlangf19@users.noreply.github.com>
2022-11-27 15:04:37 -0500

    Update Process_collection.ps1
    Added additional metadata to the process collection script, to include user, host, and domain data.

--- Collection Scripts/Process_collection.ps1
+++ Collection Scripts/Process_collection.ps1
@@ -5,8 +5,17 @@ Output:
     Type: CSV
     Attributes: Name, executable path, PID, parent PID, and creation time(all in Neo4j time formatting)
 #>
-$procs = Get-CimInstance -Class Win32_Process | Select-Object -Property name, processid,parentprocessid,ExecutablePath, creationdate
+$procs = Get-CimInstance -Class Win32_Process 
 foreach($x in $procs){
+    $owner = Invoke-CimMethod -InputObject $x -MethodName GetOwner | Select-Object -ExpandProperty User
+    $hostname = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty name
+    $domain = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty domain
+    $x | Add-Member $hostname -Name hostname -MemberType NoteProperty
+    $x | Add-Member $domain -Name domain -MemberType NoteProperty
+    $x | Add-Member $owner -Name user -MemberType NoteProperty
     $x | Add-Member (('{0:yyyyMMdd}' -f $x.creationdate).ToString() + "T" + ('{0:HH:mm}' -f $x.creationdate).ToString() + "Z") -Name neo4jcreationtime -MemberType NoteProperty
 }
-$procs | Export-Csv -Path 'C:\Users\jlang\Documents\HES\CSCI E-59\Final Project\Data Sources\processes.csv'
+$procs = $procs | Select-Object -Property name, processid,parentprocessid,ExecutablePath,creationdate,user,hostname,domain,neo4jcreationtime
+# $procs | Export-Csv -Path 'C:\Users\jlang\Documents\HES\CSCI E-59\Final Project\Data Sources\processes.csv'
+$json_procs = ConvertTo-Json -InputObject $procs
+$json_procs | Out-File '\\TRUENAS\data\BACKUP\Storage\IR Graph Project\Collection Scripts\Development\processes.json'
