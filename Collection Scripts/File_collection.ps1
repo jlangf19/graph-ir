e0b5721481bf96901d0f2430fa84206ec1455d56
jlangf19 <108038851+jlangf19@users.noreply.github.com>
2022-11-28 21:34:16 -0500

    Update File_collection.ps1
    Updated file collection to include additional host and file properties such as mode, host, and domain.

--- Collection Scripts/File_collection.ps1
+++ Collection Scripts/File_collection.ps1
@@ -8,13 +8,16 @@ Output:
 $files = ls | Where-Object -Property Attributes -eq 'Archive'
 foreach($x in $files){
     $x| Add-Member ($x.Directory.ToString() + "\" + $x.Name.ToString()) -Name Path -MemberType NoteProperty
+    $hostname = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty name
+    $domain = Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty domain
+    $x | Add-Member $hostname -Name hostname -MemberType NoteProperty
+    $x | Add-Member $domain -Name domain -MemberType NoteProperty
     $y = Get-FileHash $x -Algorithm SHA1
     $x | Add-Member $y.Hash -Name Hash -MemberType NoteProperty
     $x | Add-Member (('{0:yyyyMMdd}' -f $x.CreationTimeUtc).ToString() + "T" + ('{0:HH:mm}' -f $x.CreationTimeUtc).ToString() + "Z") -Name neo4jcreationtime -MemberType NoteProperty
     $x | Add-Member (('{0:yyyyMMdd}' -f $x.LastAccessTimeUtc).ToString() + "T" + ('{0:HH:mm}' -f $x.LastAccessTimeUtc).ToString() + "Z") -Name neo4jlastaccesstime -MemberType NoteProperty
     $x | Add-Member (('{0:yyyyMMdd}' -f $x.LastWriteTimeUtc).ToString() + "T" + ('{0:HH:mm}' -f $x.LastWriteTimeUtc).ToString() + "Z") -Name neo4jlastwritetime -MemberType NoteProperty
 }
-$files | Select-Object -Property Name,Path,Extension,neo4jcreationtime,neo4jlastaccesstime,neo4jlastwritetime,hash | Export-Csv -Path 'C:\Users\jlang\Documents\HES\CSCI E-59\Final Project\Data Sources\files.csv'
-
-
-
+$files = $files | Select-Object -Property Name,Path,Extension,hostname,domain,neo4jcreationtime,neo4jlastaccesstime,neo4jlastwritetime,hash,mode,length,exists
+$json_files = ConvertTo-Json -InputObject $files
+$json_files | Out-File '\\TRUENAS\data\BACKUP\Storage\IR Graph Project\Collection Scripts\Development\files.json'
