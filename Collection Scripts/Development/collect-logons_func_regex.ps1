11ffc9ff667654476516d20503d2b8ccaac98b6a
jlangf19 <108038851+jlangf19@users.noreply.github.com>
2023-02-27 13:00:57 -0500

    Update collect-logons_func_regex.ps1
    Added the completed parsing of the logon event (EID 4624).

--- Collection Scripts/Development/collect-logons_func_regex.ps1
+++ Collection Scripts/Development/collect-logons_func_regex.ps1
@@ -1,54 +1,20 @@
-﻿# Functions
-
-# Parses log fields into an array containing a key value pair
-Function Parse-Line {
-    Param(
-        $Value
-    ) 
-    $Value = $Value -split ":", 2
-    $Value[0] = $Value[0].Trim()
-    $Value[1] = $Value[1].Trim()
-    $Value = @{$Value[0] = $Value[1]}
-    $Value
-}
-
 # Performs the parse and enrichment of the logon
 Function Parse-LogonObject {
     Param(
         $Logon
     )
-    # Indicates the positions within a Powershell logon event message that contain information of value
-    # Note: index values inconsistent with previous identified relevant keys (24->22, 28->24, 30->
-    # $reg = 'Logon Type:\s*?(?<logon_type>\d)|Virtual Account:\s*(?<virtual_account>%%\d{4})|Elevated Token:\s*?(?<elevated_token>%%\d{4})|Impersonation Level:\s*?(?<impersonation_level>%%\d{4})|Security ID:\s*?(?<security_id>S-\d-\d-\d{2})'
-    $reg = '(?s)Logon Type:\s*?(?<logon_type>\d+).*?
-            Virtual Account:\s*?(?<virtual_account>%%\d{4}).*?
-            Elevated Token:\s*?(?<elevated_token>%%\d{4}).*?
-            Impersonation Level:\s*?(?<impersonation_level>%%\d{4}).*?
-            Security ID:\s*?(?<sid>S-\d+-\d+-\d+).*?
-            Account Name:\s*?(?<account_name>\w.*?)\s*?
-            Account Domain:\s*?(?<account_domain>\w.*?)\s*?
-            Logon ID:\s*?(?<logon_id>0x\d.*?)\s'
-    $value_index = @(9,11,12,14,17,18,19,20,21,22,23,27,28,31,32,33,36,37,38)
+    $reg = '(?s)Logon Type:\s*?(?<logon_type>\d+).*?Virtual Account:\s*?(?<virtual_account>%%\d{4}).*?Elevated Token:\s*?(?<elevated_token>%%\d{4}).*?Impersonation Level:\s*?(?<impersonation_level>%%\d{4}).*?Security ID:\s*?(?<sid>S-\d+-\d+-\d+).*?Account Name:\s*?(?<account_name>\S.*?)\s*?Account Domain:\s*?(?<account_domain>\S.*?)\s*?Logon ID:\s*?(?<logon_id>0x\d.*?)\s*?Linked Logon ID:\s*?(?<linked_logon_id>0x\d.*?)\s*?Network Account Name:\s*?(?<network_account_name>\S.*?)\s*?Network Account Domain:\s*?(?<network_account_Domain>\S.*?).*?Logon GUID:\s*?(?<logon_gui>\S.*?)\s.*?Process ID:\s*?(?<pid>0x\d.*?)\s*?Process Name:\s*?(?<process_name>\S.*?)\s\s.*?Workstation Name:\s*?(?<logon_hostname>\S.*?)\s*?Source Network Address:\s*?(?<source_address>\S.*?)\s*?Source Port:\s*?(?<source_address>\S.*?).*?Logon Process:\s*?(?<logon_processes>\S.*?)\s'
     $vals = @{time = $Logon.TimeGenerated; hostname = $Logon.MachineName}
     $message = $Logon.Message
-    $message = $message.Split([Environment]::NewLine)
-    foreach ($index in $value_index) {
-        $val = Parse-Line -Value $message[$index]
-        # Checks for values requiring conversion from the msobj.dll format (Virtual Account, Elevated Token, Impersonation Level)
-        if($index -in 11,12,14){
-            $temp_key = $val.GetEnumerator().Name
-            $temp_val = $val[$temp_key]
-            $temp_val = $msobj_hash[$temp_val]
-            $val[$temp_key] = $temp_val
-        }
-        elseif($index -in 20,21,27) {
-            $temp_key = $val.GetEnumerator().Name
-            $temp_val = $val[$temp_key]
-            $temp_val = [uint32]$temp_val
-            $val[$temp_key] = $temp_val
-        }
-        $vals += $val
+    $val = $message -match $reg
+    foreach ($property in 'elevated_token','virtual_account','impersonation_level') {
+        $Matches.$property = $msobj_hash[$Matches.$property]
     }
+    foreach ($property in 'pid','logon_id','linked_logon_id') {
+        $Matches.$property = [uint32]$Matches.$property
+    }
+    $Matches.Remove(0)
+    $vals += $Matches
     $logon_object = New-Object PSObject -Property $vals
     $logon_object
 }
@@ -61,5 +27,10 @@ $msobj_hash = @{"%%1832" = "Identification"; "%%1833" = "Impersonation";
                 "%%1847" = "DisallowMmConfig"; "%%1848" = "Off"; "%%1849" = "Auto"
 }
 
-$event = Import-Csv .\log.csv
-$logon_obj = Parse-LogonObject -Logon $event
+$logons = Get-EventLog -LogName Security -InstanceId 4624
+$logon_list = @()
+foreach ($event in $logons){
+    $logon_obj = Parse-LogonObject -Logon $event
+    $logon_list += $logon_obj
+}
+$logon_list | ConvertTo-Csv -NoTypeInformation | Set-Content -path '\\TRUENAS\data\BACKUP\Storage\IR Graph Project\Collection Scripts\Data\logons.csv'
